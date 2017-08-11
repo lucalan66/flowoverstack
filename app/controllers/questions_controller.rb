c95fbaddb5edc11aaaa35155b637d503d2bd286d
santiandres33 <santiandres33@hotmail.com>
2017-08-10 20:53:11 -0500

    crud hasta edit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,35 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
+  def index
+  	@questions = Question.all
+  end
+
+  def show
+ 
+  	@question = Question.find(params[:id])
+  end
+
+  def new
+  	@question = Question.new
+  end
+
+  def create
+  	@question = current_user.questions.create!(question_params)
+  	redirect_to action: "index"
+  end
+
+  def edit
+  	@question = Question.find(params[:id])
+  end
+
+  def destroy
+  	@question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  # get 'questions/index'
+
+  # get 'questions/show'
+
+  # get 'questions/new'
+
+  # get 'questions/edit'
+
+  # post 'questions/create'
+
+  resources :questions
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'home#index'
